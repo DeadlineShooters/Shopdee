2b9c1d3788999c82c9d40195cc8cf6becb0e995f
Ngoc <dungtuyetngoc@gmail.com>
2023-11-04 18:28:26 +0700

    finished bottom nav

--- ShopDee-front-end/App.js
+++ ShopDee-front-end/App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-export default function App() {
+import Home from "./src/pages/Home";
+import MyOrders from "./src/pages/MyOrders";
+import Me from "./src/pages/Me";
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#007EA7",
+        tabBarInactiveTintColor: "black",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            if (focused)
+              return <Ionicons name="ios-home-sharp" size={24} color={color} />;
+            else
+              return (
+                <Ionicons name="ios-home-outline" size={24} color={color} />
+              );
+          },
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="My Orders"
+        component={MyOrders}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            return <FontAwesome5 name="list-alt" size={24} color={color} />;
+          },
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Me"
+        component={Me}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            if (focused)
+              return <Ionicons name="ios-person" size={24} color={color} />;
+            else
+              return (
+                <Ionicons name="ios-person-outline" size={24} color={color} />
+              );
+          },
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  let loggedIn = false;
+
+  if (loggedIn) return <HomeScreen />;
+  else
+    return (
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    );
+}
---
+++ ShopDee-front-end/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+//  testing code please replace!!
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
