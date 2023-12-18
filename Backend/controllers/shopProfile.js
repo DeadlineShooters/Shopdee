9b68dc7b65ee32746ad94d3a4d28e6cde0d3c829
Lock121 <119125227+Lock121@users.noreply.github.com>
2023-12-18 22:00:59 +0700

    ShopProfile controller

--- Backend/controllers/shopProfile.js
+++ Backend/controllers/shopProfile.js
@@ -6,17 +6,18 @@ import crypto from "crypto"
 
 export const createShop = async (req, res) => {
     try {
-        const {image, shopName, email, phone, address} = req.body;
-        const existingUser = await shop.findOne({email});
+        const {shopName, email, phone, address} = req.body;
+        const existingUser = await Shop.findOne({email});
         if (existingUser)
         {
             return res.status(400).json({messages: "Email has been already registered!"});
         }
-        const newShop = new user({shopName, email, phone, address});
+        const newShop = new Shop({shopName, email, phone, address});
         await newShop.save();
+        res.status(201).json({ message: "Shop profile created successfully" });
     }
     catch (error) {
-        console.log("error registering user", error);
+        console.log("error registering shop", error);
         res.status(500).json({messages: "Registration failed"});
     }
 }
@@ -28,35 +29,43 @@ const generateSecretKey = () => {
 
 const secretKey = generateSecretKey();
 
-export const signIn = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        //Check if the user exists
-        const loginUser = await user.findOne({email});
-        if (!loginUser)
-        {
-            return res.status(401).json({message: "Invalid email or password"});
-        }
-        if (loginUser.password != password)
-        {
-            return res.status(401).json({message: "Invalid password"});
-        }
-        const token = jwt.sign({userID:loginUser._id}, secretKey);
-        res.status(200).json({token})
-    } catch (error) {
-        res.status(500).json({message: "Login failed"});
-    }
-}
 
 export const getprofile = async (req, res) => {
     try {
-        const findUserID = req.params.userID;
-        const User = await user.findById(findUserID);
-        if (!User) {
-            res.status(404).json({message: "User not found"});
+        const findUserID = req.params.shopID;
+        const shop = await Shop.findById(findUserID);
+        if (!shop) {
+            res.status(404).json({message: "Shop not found"});
         }
-        res.status(200).json({User});
+        res.status(200).json({shop});
     } catch (error) {
-        res.status(500).json({message: "Error retrieving the user profile"});
+        res.status(500).json({message: "Error retrieving the shop profile"});
     }
 }
+export const updateShopProfile = async (req, res) => {
+    console.log(req.body);
+    try {
+        const shopID = req.params.shopID; // Assuming you have the shop ID in the request parameters.
+        const { shopName, email, phone, address, profilePic } = req.body;
+
+        await shop.findByIdAndUpdate(shopID, {
+            shopName: shopName,
+            email: email,
+            phone: phone,
+            address: address,
+            profilePic: {
+                public_id: profilePic.publicId,
+                url: profilePic.secureUrl
+            }
+            // Add other fields as needed for updating the shop profile
+        });
+
+        res.status(200).send({
+            success: true,
+            message: "Shop profile updated successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error updating the shop profile" });
+    }
+};
--- Backend/routes/shopProfile.js
+++ Backend/routes/shopProfile.js
@@ -1,8 +1,9 @@
 import express from "express";
-
+import {singleUpload} from "../middlewares/multer.js"
 import * as shopProfile from '../controllers/shopProfile.js'
 const router = express.Router();
-
 router.post('/register', shopProfile.register);
-router.get('/profile/:userID', shopProfile.getprofile);
+router.get('/shopProfile/:userID', shopProfile.getprofile); 
+router.put('/profile/update/:userID', singleUpload, user.updateprofile);
 export default router;
+
