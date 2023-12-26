97c330db0e2e5c8ad1d6a0865805a93c5dd4e766
Ngoc <dungtuyetngoc@gmail.com>
2023-12-26 17:23:39 +0700

    remove comments

--- Backend/controllers/buyerProduct.js
+++ Backend/controllers/buyerProduct.js
@@ -6,11 +6,9 @@ controller.showAll = async (req, res) => {
     // Fetch all products
     const products = await Product.find().populate(["category", "shop"]);
 
-    // Send the products as a JSON response
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
-    // Handle the error and send an appropriate response
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
