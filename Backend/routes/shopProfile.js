d0682d588d1d3c657fefda07352dcfd6dbe4517f
999Kit <98326962+999Kit@users.noreply.github.com>
2024-01-05 08:41:19 +0700

    removed some files

--- Backend/routes/shopProfile.js
+++ Backend/routes/shopProfile.js
@@ -1,8 +1,7 @@
 import express from "express";
-import {singleUpload} from "../middlewares/multer.js"
 import * as shopProfile from '../controllers/shopProfile.js'
 const router = express.Router();
 router.post('/createShop', shopProfile.registerShop);
 router.get('/shopProfile/:shopID', shopProfile.getprofile); 
-router.put('/shopProfile/:shopID/update', singleUpload, shopProfile.updateShopProfile);
+router.put('/shopProfile/:shopID/update', shopProfile.updateShopProfile);
 export default router;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { singleUpload } from "../middlewares/multer.js";
 import * as user from "../controllers/users.js";
 const router = express.Router();
 
@@ -9,7 +8,7 @@ router.post("/reset-password", user.resetPassword);
 router.post("/reset-password/verify-token", user.verifyToken);
 router.get("/profile/:userID", user.getprofile);
 router.post("/profile/checkShopOwner", user.checkShopOwner);
-router.put("/profile/update/:userID", singleUpload, user.updateprofile);
+router.put("/profile/update/:userID", user.updateprofile);
 router.put("/profile/set-address/:userID", user.updateaddress);
 router.post("/profile/update-password", user.updatePassword);
 export default router;
